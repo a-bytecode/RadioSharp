77847ea4344cb5e3b3aeada8ceb538a56997a296
Alexander Astrachanzev <alexejastrein@gmail.com>
2023-01-03 15:07:12 +0100

    style: build layout

---
+++ app/src/main/java/com/example/radiosharp/MainViewModel.kt
@@ -0,0 +1,10 @@
+package com.example.radiosharp
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.ViewModel
+
+class MainViewModel(application: Application) : AndroidViewModel(application) {
+
+
+}
---
+++ app/src/main/java/com/example/radiosharp/ui/HomeFragment.kt
@@ -0,0 +1,30 @@
+package com.example.radiosharp.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.radiosharp.databinding.HomeFragmentBinding
+
+class HomeFragment : Fragment() {
+
+
+    private lateinit var binding: HomeFragmentBinding
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        binding = HomeFragmentBinding.inflate(inflater)
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
