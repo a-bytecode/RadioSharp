a99c415cf09f859196739fc9d942c281e59b57db
Alexander Astrachanzev <alexejastrein@gmail.com>
2023-01-06 17:48:21 +0100

    fix: Absturz Problem beim Laden des Senders beseitigt.

--- app/src/main/java/com/example/radiosharp/ui/DetailFragment.kt
+++ app/src/main/java/com/example/radiosharp/ui/DetailFragment.kt
@@ -18,12 +18,15 @@ import androidx.core.net.toUri
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
+import androidx.lifecycle.lifecycleScope
 import coil.load
 import com.bumptech.glide.Glide
 import com.example.radiosharp.MainViewModel
 import com.example.radiosharp.R
 import com.example.radiosharp.databinding.DetailFragmentBinding
 import com.example.radiosharp.model.RadioClass
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class DetailFragment: Fragment() {
 
@@ -70,7 +73,7 @@ class DetailFragment: Fragment() {
                viewModel.fillText(binding.genreTextDetail)
 
                //TODO Starten von animierten gifs in der Detailansicht
-               val gif = ContextCompat.getDrawable(requireContext(),R.drawable.giphy4) as AnimatedImageDrawable
+               val gif = ContextCompat.getDrawable(requireContext(),R.drawable.giphy3) as AnimatedImageDrawable
                gif.start()
 
                Glide.with(requireContext())
@@ -82,17 +85,20 @@ class DetailFragment: Fragment() {
                binding.playButton.setOnClickListener {
                    viewModel.buttonAnimator(binding.playButton)
                    stopPlaying()
-                   mediaPlayer = MediaPlayer().apply {
-                       setAudioAttributes(
-                           AudioAttributes.Builder()
-                               .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
-                               .setUsage(AudioAttributes.USAGE_MEDIA)
-                               .build()
-                       )
-                   }
-                   mediaPlayer!!.setDataSource(requireContext(),currentStation.playRadio.toUri())
-                   mediaPlayer!!.prepare()
-                   mediaPlayer!!.start()
+
+                       mediaPlayer = MediaPlayer().apply {
+                           setAudioAttributes(
+                               AudioAttributes.Builder()
+                                   .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                                   .setUsage(AudioAttributes.USAGE_MEDIA)
+                                   .build()
+                           )
+                       }
+                       mediaPlayer!!.setDataSource(requireContext(),currentStation.playRadio.toUri())
+                       mediaPlayer!!.prepareAsync()
+                       mediaPlayer!!.setOnPreparedListener {
+                           it.start()
+                       }
                }
                binding.stopButton.setOnClickListener {
                    viewModel.buttonAnimator(binding.stopButton)
